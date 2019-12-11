cce2672323aef498fabac79aa29e8e5c4fa775fe
Rahmat <akintolarahmah@gmail.com>
2019-12-11 22:50:33 +0000

    rubocop errors fixed

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@
 
 # frozen_string_literal: true
 
-module Enumerable # rubocop:disable Metrics/ModuleLength
+module Enumerable
   def my_each
     return to_enum unless block_given?
 
@@ -36,58 +36,58 @@ module Enumerable # rubocop:disable Metrics/ModuleLength
     end
     result
   end
-# rubocop:disable  Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+#rubocop:disable  Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
   def my_all?(arg = nil)
     if block_given?
-      my_each { |elem| return false unless yield(elem)}
+      my_each { |elem| return false unless yield(elem) }
     elsif arg.class == Class
-      my_each { |elem| return false unless elem.class.ancestors.include? arg}
+      my_each { |elem| return false unless elem.class.ancestors.include? arg }
     elsif arg.class == Regexp
-      my_each { |elem| return false unless elem =~ arg}
+      my_each { |elem| return false unless elem =~ arg }
     elsif arg.nil?
-      my_each { |elem| return false unless elem}
+      my_each { |elem| return false unless elem }
     else
-      my_each { |element| return false unless elem === arg}
+      my_each { |elem| return false unless elem == arg }
     end
   end
 
   def my_any?(arg = nil)
     if block_given?
-      my_each { |elem| return true if yield(elem)}
+      my_each { |elem| return true if yield(elem) }
     elsif arg.class == Class
-      my_each { |elem| return true if elem.class.ancestors.include? arg}
+      my_each { |elem| return true if elem.class.ancestors.include? arg }
     elsif arg.class == Regexp
-      my_each { |elem| return true if elem =~ arg}
+      my_each { |elem| return true if elem =~ arg }
     elsif arg.nil?
-      my_each { |elem| return true if elem}
+      my_each { |elem| return true if elem }
     else
-      my_each { |element| return true if elem === arg}
+      my_each { |elem| return true if elem == arg }
     end
   end
 
   def my_none?(arg = nil)
     if block_given?
-      my_each { |elem| return false if yield(elem)}
+      my_each { |elem| return false if yield(elem) }
     elsif arg.class == Class
-      my_each { |elem| return false if elem.class.ancestors.include? arg}
+      my_each { |elem| return false if elem.class.ancestors.include? arg }
     elsif arg.class == Regexp
-      my_each { |elem| return false if elem =~ arg}
+      my_each { |elem| return false if elem =~ arg }
     elsif arg.nil?
-      my_each { |elem| return false if elem}
+      my_each { |elem| return false if elem }
     else
-      my_each { |element| return false if elem === arg}
+      my_each { |elem| return false if elem == arg }
     end
   end
 
-  def my_count (arg = nil)
+  def my_count(arg = nil)
     counter = 0
     if block_given?
-      my_each { |elem| counter += 1 if yield(elem)}
+      my_each { |elem| counter += 1 if yield(elem) }
     elsif arg.nil?
-      my_each { |elem| counter += 1}
+      my_each { |_elem| counter += 1}
     else
-      my_each { |elem| counter += 1 if elem == arg}
+      my_each { |elem| counter += 1 if elem == arg }
     end
     counter
   end
@@ -96,11 +96,10 @@ module Enumerable # rubocop:disable Metrics/ModuleLength
     return to_enum unless block_given?
 
     result = []
-    my_each do |element|
-      result << block.call(element)
+    my_each do |elem|
+      result << block.call(elem)
     end
     result
   end
-
 end
-# rubocop:enable  Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+#rubocop:enable  Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
